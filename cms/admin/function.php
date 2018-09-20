cfad2b4a1b0056f43df27e774aa6321099e295d1
ChristopherToken <toph.hombert@gmail.com>
2018-09-20 16:44:39 +0700

    display list post in table

--- cms/admin/function.php
+++ cms/admin/function.php
@@ -25,21 +25,21 @@ function insert_categories() {
 
 
 function findAllCategories () {
-    global $connection;
-         //BRING CATEGORIES FROM DATABASE TO TABLE IF WANT LIMIT USE: LIMIT: 3
-            $query =  "SELECT * FROM categories";
-            $select_categories = mysqli_query($connection, $query);
-                          
-            //display categories in table
-             while($row = mysqli_fetch_assoc( $select_categories)) {
-            $cat_id =  $row['cat_id'];
-            $cat_title =  $row['cat_title'];
-                echo "<tr>";
-                echo "<td>{$cat_id}</td>";
-                echo "<td>{$cat_title}</td>";
-                echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                echo "</tr>";
+global $connection;
+//BRING CATEGORIES FROM DATABASE TO TABLE IF WANT LIMIT USE: LIMIT: 3
+    $query =  "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+                    
+    //display categories in table
+    while($row = mysqli_fetch_assoc( $select_categories)) {
+    $cat_id =  $row['cat_id'];
+    $cat_title =  $row['cat_title'];
+    echo "<tr>";
+    echo "<td>{$cat_id}</td>";
+    echo "<td>{$cat_title}</td>";
+    echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
+    echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
+    echo "</tr>";
     }
 }
 
