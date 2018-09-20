45f7b8c28643863392945294a80105b4802292ff
ChristopherToken <toph.hombert@gmail.com>
2018-09-20 11:52:17 +0700

    blog already display on table

--- cms/admin/posts.php
+++ cms/admin/posts.php
@@ -17,33 +17,54 @@
         <small>Don't be stupid</small>
     </h1>
 
-    <table class="table table-bordered table-hover">
-        <thead>
-        <tr>
-            <th>Id</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Category</th>
-            <th>Stautus</th>
-            <th>Image</th>
-            <th>Tags</th>
-            <th>Comments</th>
-            <th>Date</th>
-        </tr>
-        </thead>
+<table class="table table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>Id</th>
+        <th>Author</th>
+        <th>Title</th>
+        <th>Category</th>
+        <th>Status</th>
+        <th>Image</th>
+        <th>Tags</th>
+        <th>Comments</th>
+        <th>Date</th>
+    </tr>
+    </thead>
 
-    <tbody>
-        <tr>
-            <td>10</td>
-            <td>Christopher Homberg</td>
-            <td>Photo</td>
-            <td>Boostrap</td>
-            <td>Online</td>
-            <td>Image</td>
-            <td>tags</td>
-            <td>Comments</td>
-            <td>Date</td>
-        </tr>
+<tbody>
+
+<?php 
+    //PULL POST FROM DATABASE
+    $query = "SELECT * FROM posts";
+    $select_posts = mysqli_query($connection,$query);
+
+    while($row = mysqli_fetch_assoc($select_posts)) {
+        $post_id = $row['post_id'];           
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tags = $row['post_tags'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_date = $row['post_date'];
+    }  
+    //DISPLAY POST IN TABLE
+    echo "<tr>";
+        echo "<td>{$post_id}</td>";
+        echo "<td>{$post_author}</td>";
+        echo "<td>{$post_title}</td>";
+        echo "<td>{$post_category_id}</td>";
+        echo "<td>{$post_status}</td>";
+        echo "<td>{$post_image}</td>";
+        echo "<td>{$post_tags}</td>";
+        echo "<td>{$post_comment_count}</td>";
+        echo "<td>{$post_date}</td>";
+    echo "</tr>";
+       
+?>
+     
     </tbody>
     </table> 
 
