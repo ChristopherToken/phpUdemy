702aee181b072677b6fb5db0cd8dad22f7ecbfcb
ChristopherToken <toph.hombert@gmail.com>
2018-09-24 09:22:49 +0700

    update rewrite the code

--- cms/admin/includes/add_post.php
+++ cms/admin/includes/add_post.php
@@ -1,45 +1,28 @@
-<?php 
+<?php if(isset($_POST['create_post'])) {
+
+    $post_title = $_POST['title'];
+    $post_author = $_POST['author'];
+    $post_category_id = $_POST['post_category_id'];
+    $post_status = $_POST['post_status'];
+
+    $post_image = $_FILES['image']['name'];
+    $post_iamge_temp = $_FILES['image']['tmp_name'];
 
 
-    if(isset($_POST['create_post'])) {
-
-        $post_title = $_POST['title'];
-        $post_author = $_POST['author'];
-        $post_category_id = $_POST['post_category_id'];
-        $post_status = $_POST['post_status'];
-        
-        $post_image = $_FILES['image']['name'];
-        $post_image_temp = $_FILES['image']['tmp_name'];
-        
-        $post_tags = $_POST['post_tags'];
-        $post_content = $_POST['post_content'];
-        $post_date = date('d-m-y');
-        $post_comment_count = 4;
+    $post_tags = $_POST['post_tags'];
+    $post_content = $_POST['post_content'];
+    $post_date = date('d-m-y');
+    $post_comment_count = 4;
+}  
 
 
 
-            move_uploaded_file($post_image_temp, "../images/$post_image");
 
-
-            $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_comment_count, post_status)";
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-            $query .= "VALUES({$post_category_id}, '{$post_title}', '{$post_author}', now(), '{$post_content}','{$post_tags}', '{$post_comment_count}', '{$post_status}', )";
-=======
-            $query .= "VALUES({$post_category_id}, '{$post_titel}', '{$post_author}', now(), '{$post_content}','{$post_tags}', '{$post_comment_count}', '{$post_status}', )";
->>>>>>> parent of 83cc439... update still didnt fix the bug
-=======
-            $query .= "VALUES({$post_category_id}, '{$post_titel}', '{$post_author}', now(), '{$post_content}','{$post_tags}', '{$post_comment_count}', '{$post_status}', )";
->>>>>>> parent of 83cc439... update still didnt fix the bug
-
-            $create_post_query  = myqli_query($connection, $query);
-
-            confirmQuery($create_post_query);
-    }
 
 
 ?>
+
+
 
 
 <form actoin="" method="post" enctype="multipart/form-data">
@@ -86,4 +69,3 @@
     </div>
 
     </form>
-
