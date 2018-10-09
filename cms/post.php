7686de09ea9a89c7817ba10c840b7d603ce7a9e7
ChristopherToken <toph.hombert@gmail.com>
2018-10-09 17:01:22 +0700

    update
    !empty comment

--- cms/post.php
+++ cms/post.php
@@ -66,18 +66,25 @@
       $comment_email = $_POST['comment_email'];
       $comment_content = $_POST['comment_content'];
 
-      $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-      $query .= "VALUES ($the_post_id , '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapprove',now())";
-
-      $create_comment_query = mysqli_query($connection,$query);
-      if(!$create_comment_query) {
-          die('QUERY FAILED' . mysqli_error($connection));
-      }
+    if(!empty($comment_author) && !empty($comment_email) && !empty($comment_content)  ){
+     $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+    $query .= "VALUES ($the_post_id , '{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapprove',now())";
+  
+    $create_comment_query = mysqli_query($connection,$query);
+    if(!$create_comment_query) {
+    die('QUERY FAILED' . mysqli_error($connection));
+    }
 
       $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
       $query .= "WHERE post_id = $the_post_id ";
       $update_comment_count = mysqli_query($connection, $query);
-     }
+     
+    } else {
+
+        echo "<script>alert('Field cannot be empty')</script>";
+
+    }
+}
 
 
      ?>
@@ -107,19 +114,21 @@
                 <!-- Posted Comments -->
 
                 <?php 
+                
 
-$query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-$query .= "AND comment_status = 'approved' ";
-$query .= "ORDER BY comment_id DESC ";
-$select_comment_query = mysqli_query($connection, $query);
-if(!$select_comment_query) {
 
-    die('Query Failed' . mysqli_error($connection));
- }
-while ($row = mysqli_fetch_array($select_comment_query)) {
-$comment_date   = $row['comment_date']; 
-$comment_content= $row['comment_content'];
-$comment_author = $row['comment_author'];
+            $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
+            $query .= "AND comment_status = 'approved' ";
+            $query .= "ORDER BY comment_id DESC ";
+            $select_comment_query = mysqli_query($connection, $query);
+            if(!$select_comment_query) {
+
+                die('Query Failed' . mysqli_error($connection));
+            }
+            while ($row = mysqli_fetch_array($select_comment_query)) {
+            $comment_date   = $row['comment_date']; 
+            $comment_content= $row['comment_content'];
+            $comment_author = $row['comment_author'];
 
 
                 ?>
